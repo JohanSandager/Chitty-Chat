0b04063a61c2ebee138ece63813519779f8b16dd
JohanSandager <jsbe@itu.dk>
2023-10-20 14:16:25 +0200

    Client refactor

--- client/client.go
+++ client/client.go
@@ -49,26 +49,12 @@ func ConnectToServer(server_address string) (pb.ChitChatClient, error) {
 func Chat(server_address string, user_name string, wg *sync.WaitGroup) {
 	server_connection, _ := ConnectToServer(server_address)
 
-	ctx, cancel := context.WithCancel(context.Background())
+	stream, _ := server_connection.Chat(context.Background())
 
-	stream, _ := server_connection.Chat(ctx)
-
-	IncrementAndPrintLamportTimestamp("Send connection request")
-
-	outbound_message := &pb.ConnectionRequest{
-		UserName: user_name,
-	}
-	message_container := &pb.ChitChatInformationContainer{
-		LamportTimestamp: int64(client_lamport_timestamp),
-		These: &pb.ChitChatInformationContainer_ConnectionRequest{
-			ConnectionRequest: outbound_message,
-		},
-	}
-
-	stream.Send(message_container)
+	SendConnectionRequest(user_name, stream)
 
 	go RecieveMessages(stream)
-	go SendMessage(stream, user_name, wg, cancel)
+	go SendMessage(stream, user_name, wg)
 }
 
 func RecieveMessages(stream pb.ChitChat_ChatClient) {
@@ -79,7 +65,7 @@ func RecieveMessages(stream pb.ChitChat_ChatClient) {
 
 		if err == io.EOF {
 			return
-		} else if status, ok := status.FromError(err); ok {
+		} else if status, ok := status.FromError(err); ok { // This error handeling was helped by ChatGPT
 			if status.Code() == codes.Unavailable {
 				log.Fatalf("Terminated ungracefully: %v", status.Message())
 			}
@@ -89,47 +75,91 @@ func RecieveMessages(stream pb.ChitChat_ChatClient) {
 
 		SetAndPrintLamportTimestamp("Validate timestamp", ValidateLamportTimestamp(client_lamport_timestamp, int(inbound_message.GetLamportTimestamp())))
 
-		log.Printf(inbound_message.GetMessage().GetUserName() + ": " + inbound_message.GetMessage().GetMessage())
+		PrintMessage(inbound_message.GetMessage())
 	}
 }
 
-func SendMessage(stream pb.ChitChat_ChatClient, user_name string, wg *sync.WaitGroup, cancel context.CancelFunc) {
+func SendMessage(stream pb.ChitChat_ChatClient, user_name string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		input := scanner.Text()
-
-		IncrementAndPrintLamportTimestamp("Publish message")
-
-		if input == "c" {
-			outbound_message := &pb.DisconnectionRequest{
-				UserName: user_name,
-			}
-			message_container := &pb.ChitChatInformationContainer{
-				LamportTimestamp: int64(client_lamport_timestamp),
-				These:            &pb.ChitChatInformationContainer_DisconnectionRequest{DisconnectionRequest: outbound_message},
-			}
-
-			stream.Send(message_container)
-			stream.CloseSend()
-			time.Sleep(time.Millisecond * 100)
+		if input := scanner.Text(); input == "c" {
+			SendDisconnectionRequest(user_name, stream)
 			return
+		} else if len(input) <= 128 {
+			Publish(stream, user_name, input)
+		} else if len(input) > 128 {
+			log.Printf("Message length expected under 128 characters, actual: %v", len(input))
+		} else {
+			log.Fatal("Something went wrong")
 		}
+	}
+}
 
-		outbound_message := &pb.ChitChatMessage{UserName: user_name, Message: input}
-		message_container := &pb.ChitChatInformationContainer{
-			LamportTimestamp: int64(client_lamport_timestamp + 1),
-			These:            &pb.ChitChatInformationContainer_Message{Message: outbound_message},
-		}
+func Publish(stream pb.ChitChat_ChatClient, user_name string, input string) {
+	IncrementAndPrintLamportTimestamp("Publish message")
+	message := CreateChitChatMessageObject(user_name, input)
+	stream.Send(message)
+}
 
-		stream.Send(message_container)
+func PrintMessage(message *pb.ChitChatMessage) {
+	log.Printf(message.GetUserName() + ": " + message.GetMessage())
+}
+
+func SendConnectionRequest(user_name string, stream pb.ChitChat_ChatClient) {
+	IncrementAndPrintLamportTimestamp("Send connection request")
+	message := CreateConnectionRequestObject(user_name)
+	if err := stream.Send(message); err != nil {
+		log.Fatalf("Failed to send connection request: %v", err)
+	}
+}
+
+func SendDisconnectionRequest(user_name string, stream pb.ChitChat_ChatClient) {
+	message := CreateDisconnectionRequestObject(user_name)
+	if err := stream.Send(message); err != nil {
+		log.Fatalf("Failed to send disconnection request: %v", err)
 	}
 
 	if err := stream.CloseSend(); err != nil {
 		log.Fatalf("Failed to close the send stream: %v", err)
 	}
+
+	time.Sleep(time.Millisecond * 100)
+}
+
+func CreateChitChatMessageObject(user_name string, input string) *pb.ChitChatInformationContainer {
+	outbound_message := &pb.ChitChatMessage{UserName: user_name, Message: input}
+	message_container := &pb.ChitChatInformationContainer{
+		LamportTimestamp: int64(client_lamport_timestamp + 1),
+		These:            &pb.ChitChatInformationContainer_Message{Message: outbound_message},
+	}
+	return message_container
+}
+
+func CreateDisconnectionRequestObject(user_name string) *pb.ChitChatInformationContainer {
+	outbound_message := &pb.DisconnectionRequest{
+		UserName: user_name,
+	}
+	message_container := &pb.ChitChatInformationContainer{
+		LamportTimestamp: int64(client_lamport_timestamp),
+		These:            &pb.ChitChatInformationContainer_DisconnectionRequest{DisconnectionRequest: outbound_message},
+	}
+	return message_container
+}
+
+func CreateConnectionRequestObject(user_name string) *pb.ChitChatInformationContainer {
+	outbound_message := &pb.ConnectionRequest{
+		UserName: user_name,
+	}
+	message_container := &pb.ChitChatInformationContainer{
+		LamportTimestamp: int64(client_lamport_timestamp),
+		These: &pb.ChitChatInformationContainer_ConnectionRequest{
+			ConnectionRequest: outbound_message,
+		},
+	}
+	return message_container
 }
 
 func ValidateLamportTimestamp(client_timestamp int, server_timestamp int) int {
